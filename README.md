e8620f742c571a45ed8a18ad81f46f0953efbedb
json <json@cbx.io>
2018-09-10 05:58:41 +0800

    add rxjs

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 
 ## description 
   rxjs introduction by implementation of Observer Pattern and Iterator Pattern with ES6 js
+
+## usage for rxjs
+
+### installation
+    npm i rxjs -S
+
+### ES5 common js
+
+```code
+const { Observable, Subject, ReplaySubject, from, of, range } = require('rxjs');
+const { map, filter, switchMap } = require('rxjs/operators');
+
+range(1, 200)
+  .pipe(filter(x => x % 2 === 1), map(x => x + x))
+  .subscribe(x => console.log(x));
+```
+
+### ES6 
+
+```code
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
+
+range(1, 200)
+  .pipe(filter(x => x % 2 === 1), map(x => x + x))
+  .subscribe(x => console.log(x));
+```
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Producer from './observerPattern/Producer';
 import IteratorFromArray from './iteratorPattern/IteratorFromArray';
 import {getNumbers} from './lazyEval/getNumbers';
+import { Observable, of, from, interval, timer } from 'rxjs';
+import {Promise} from 'es6-promise';
 console.time('Observable example');
 let egghead = new Producer();
 function listener1(message) {
@@ -36,3 +38,61 @@ console.log(iteratorLazy.next());
 // { value: 4, done: false }
 console.log(iteratorLazy.next());
 console.timeEnd('lazy eval1');
+
+// console.time('observable example');
+// let observer = Observable.create(function(observer) {
+//   observer.next('Jerry');
+//   observer.next('Anna');
+//   setTimeout(() => {
+//     observer.next('RxJS 30 days!');
+//   }, 30);
+// });
+
+// console.log('start');
+// observer.subscribe((value)=>{
+//   console.log(value);
+// });
+// console.log('end');
+// console.timeEnd('observable example');
+
+
+// console.time('Observer Example');
+// let observable = Observable.create((observer)=>{
+//   try {
+//     observer.next('first value');
+//     observer.next('second value');
+//     throw 'some exception';
+//   } catch(e) {
+//     observer.error(e);
+//   }
+// // });
+// console.time('Observer Example');
+// let observable = of('Jerry', 'Anna');
+
+// console.time('Observer Example');
+// const arr = ['Jerry', 'Anna', 2016, 2017, '30 days'] 
+// let observable = from('測試');
+console.time('Observer Example');
+// let observable = from(new Promise((resolve, reject)=>{
+//   setTimeout(()=> {
+//     resolve('after 3 seconds');
+//   }, 3000);
+// }));
+let observable = timer(1000,5000);
+let observerObj = {
+  next: (value) => {
+    console.log(value);
+  },
+  error: (error) => {
+    console.log('Error', error);
+  },
+  complete: () => {
+    console.log('complete');
+  }
+}
+
+let subscription = observable.subscribe(observerObj);
+setTimeout(()=>{
+  subscription.unsubscribe();
+}, 5000);
+console.timeEnd('Observer Example');
